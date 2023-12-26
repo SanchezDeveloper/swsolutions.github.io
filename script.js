8ab31aec6a947203ebc456577181b91eefe9bf9c
SanchezDeveloper <vinicsanchez@gmail.com>
2023-12-26 18:44:24 -0300

    organizing files

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) { 
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src= "../images/barra-de-menu.png";
+        document.querySelector('.icon').src= "images/barra-de-menu.png";
     } else {
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src= "../images/marca-x.png";
+        document.querySelector('.icon').src= "images/marca-x.png";
     }
 }
 
