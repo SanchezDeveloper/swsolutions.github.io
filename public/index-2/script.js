a5707b6cbf63081033073758ba0d240f2cf5e347
SanchezDeveloper <vinicsanchez@gmail.com>
2023-12-26 18:18:51 -0300

    files organization

---
+++ public/index-2/script.js
@@ -0,0 +1,31 @@
+//menumobile
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) { 
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src= "../images/barra-de-menu.png";
+    } else {
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src= "../images/marca-x.png";
+    }
+}
+
+
+//slide vanilla JS
+let slides = document.querySelectorAll('.slide-container');
+
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+//setInterval(next, 8000);
